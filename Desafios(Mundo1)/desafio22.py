1592cebb3ee8733ff304fa5128d9203276d5524b
Davi Moreno <146610742+davi32silva@users.noreply.github.com>
2024-07-23 19:22:11 -0300

    Atualização
    Atualização

---
+++ Desafios(Mundo1)/desafio22.py
@@ -0,0 +1,7 @@
+nome = str(input('Nome: ')).strip()
+print('Analisando...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras, sem espaços'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome tem {}'.format(separa [0], len(separa[0])))
